8fa28e0e3c95066251fad9dc3c9c2dc5ec48351f
HariChaithanya <143056895+HariChaithanya@users.noreply.github.com>
2023-11-10 09:29:19 -0600

    added put and get by id mehthods

--- controllers/guitar.js
+++ controllers/guitar.js
@@ -16,9 +16,9 @@ exports.guitar_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: guitar delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.guitar_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: guitar update PUT' + req.params.id);
-};
+// exports.guitar_update_put = function(req, res) {
+//  res.send('NOT IMPLEMENTED: guitar update PUT' + req.params.id);
+// };
 
 // List of all Costumes
 exports.guitar_list = async function(req, res) {
@@ -31,8 +31,7 @@ exports.guitar_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     } 
    };
-   
-   // VIEWS
+     // VIEWS
 // Handle a show all view
 exports.guitar_view_all_Page = async function(req, res) {
     try{
@@ -64,3 +63,35 @@ exports.guitar_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     } 
    }
+
+   // for a specific eagle.
+exports.guitar_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await guitar.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    exports.guitar_update_put = async function(req, res) {
+        console.log(`update on id ${req.params.id} with body
+        ${JSON.stringify(req.body)}`)
+        try {
+        let toUpdate = await guitar.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.guitar_color)
+        toUpdate.guitar_color = req.body.guitar_color;
+        if(req.body.guitar_type) toUpdate.guitar_type = req.body.guitar_type;
+        if(req.body.guitar_price) toUpdate.guitar_price = req.body.guitar_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+        } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
+        }
+        };
+ 
