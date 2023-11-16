85ef305126b8b5473f6ad2d986164827f9b42284
HariChaithanya <143056895+HariChaithanya@users.noreply.github.com>
2023-11-15 19:42:24 -0600

    added styles and guitarDeataills by id

--- routes/guitar.js
+++ routes/guitar.js
@@ -3,4 +3,5 @@ const guitar_controlers= require('../controllers/guitar');
 var router = express.Router();
 /* GET costumes */
 router.get('/', guitar_controlers.guitar_view_all_Page );
+router.get('/detail', guitar_controlers.guitar_view_one_Page);
 module.exports = router;
