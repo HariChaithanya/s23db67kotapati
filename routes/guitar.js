90c8bde2fddadd42200d2d9dc272cccc5a83654f
HariChaithanya <143056895+HariChaithanya@users.noreply.github.com>
2023-11-15 19:58:54 -0600

    added create functionality

--- routes/guitar.js
+++ routes/guitar.js
@@ -4,4 +4,7 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', guitar_controlers.guitar_view_all_Page );
 router.get('/detail', guitar_controlers.guitar_view_one_Page);
+router.get('/create', guitar_controlers.guitar_create_Page);
+/* GET create update page */
+router.get('/update', costume_controlers.costume_update_Page)
 module.exports = router;
