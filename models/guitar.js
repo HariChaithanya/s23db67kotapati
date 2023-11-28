d13c6ae63d00dd00e097522d5bc308d52d101655
HariChaithanya <143056895+HariChaithanya@users.noreply.github.com>
2023-11-28 16:52:25 -0600

    added authenication

--- models/guitar.js
+++ models/guitar.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const guitarSchema = mongoose.Schema({
     guitar_type: String,
-    guitar_price: Number,
+    guitar_price: {
+        type: Number,
+        min: [0, 'The guitar price must be greater than or equal to 0'], 
+        max: [4500, 'The guitar price cannot exceed 4500'], 
+        required: [true, 'guitar price is required'] 
+    },
     guitar_color: String
 })
 module.exports = mongoose.model("guitar", guitarSchema)
--- routes/guitar.js
+++ routes/guitar.js
@@ -1,11 +1,22 @@
 var express = require('express');
 const guitar_controlers= require('../controllers/guitar');
 var router = express.Router();
+var passport=require('passport');
 /* GET costumes */
 router.get('/', guitar_controlers.guitar_view_all_Page );
 router.get('/detail', guitar_controlers.guitar_view_one_Page);
 router.get('/create', guitar_controlers.guitar_create_Page);
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+router.get('/update', secured,guitar_controlers.guitar_update_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+   });
 /* GET create update page */
-router.get('/update', guitar_controlers.guitar_update_Page)
+//router.get('/update', guitar_controlers.guitar_update_Page)
 router.get('/delete', guitar_controlers.guitar_delete_Page);
 module.exports = router;
